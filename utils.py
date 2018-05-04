6fbba8ef0f612c166bca385636286d3fbf9db297
Raul Puri <raulpuric@berkeley.edu>
2018-05-04 10:14:30 -0700

    mask utils update for 0.4 cuda

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import torch
 
 def get_mask_from_lengths(lengths):
     max_len = torch.max(lengths)
-    ids = torch.arange(0, max_len, out=torch.LongTensor(max_len)).cuda()
+    ids = torch.arange(0, max_len).long().cuda()
     mask = (ids < lengths.unsqueeze(1)).byte()
     return mask
 

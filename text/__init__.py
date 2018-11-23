88eb41337adfb6033057714f5554fcc453685868
Yeongtae <hwak1234@hanmail.net>
2018-11-23 17:43:03 +0900

    update cleaner

--- text/__init__.py
+++ text/__init__.py
@@ -98,3 +98,4 @@ if __name__ == "__main__":
   print(text_to_sequence('테스트 문장입니다.? ', ['korean_cleaners']))
   print(_clean_text('AB테스트 문장입니다.? ', ['korean_cleaners']))
   print(_clean_text('mp3 파일을 홈페이지에서 다운로드 받으시기 바랍니다.',['korean_cleaners']))
+  print(_clean_text("마가렛 대처의 별명은 '철의 여인'이었다.", ['korean_cleaners']))

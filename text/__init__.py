e0c7e0e9280b47a8a9cc81f8bc11f41cc7f6a495
rafaelvalle <rafaelvalle@nvidia.com>
2018-11-26 16:41:21 -0800

    adding changes to text

--- text/__init__.py
+++ text/__init__.py
@@ -37,8 +37,6 @@ def text_to_sequence(text, cleaner_names):
     sequence += _arpabet_to_sequence(m.group(2))
     text = m.group(3)
 
-  # Append EOS token
-  sequence.append(_symbol_to_id['~'])
   return sequence
 
 

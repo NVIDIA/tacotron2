0a07331e0e8eba86307a0bc5f03db884dc1b296e
Yeongtae <hwak1234@hanmail.net>
2018-11-30 16:49:48 +0900

    korean train test

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 from scipy.io.wavfile import read
-from scipy import signal
 import librosa
 import torch
 
@@ -13,11 +12,15 @@ def get_mask_from_lengths(lengths):
     return mask
 
 def load_wav_to_torch(full_path, sr):
+    ## make learning fast
+    #sampling_rate, data = read(full_path, sr)
+
+    # make learning slow
     data, sampling_rate = librosa.core.load(full_path, sr)
+    data=data*max_wav_value
     assert sr == sampling_rate, "{} SR doesn't match {} on path {}".format(
         sr, sampling_rate, full_path)
     return torch.FloatTensor(data.astype(np.float32))
-
 
 def load_filepaths_and_text(filename, sort_by_length, split="|"):
     with open(filename, encoding='utf-8') as f:

a520f430219d26acdcbf1ebaad211284eaa69d42
Yeongtae <hwak1234@hanmail.net>
2018-12-03 12:09:07 +0900

    add Trim

--- change_sr.py
+++ change_sr.py
@@ -5,6 +5,12 @@ sr = 22050
 max_wav_value=32768.0
 file_list = ['filelists/nam-h_test_filelist.txt', 'filelists/nam-h_train_filelist.txt', 'filelists/nam-h_val_filelist.txt']
 
+# M-AILABS (and other datasets) trim params
+trim_fft_size = 1024
+trim_hop_size = 256
+trim_top_db = 23
+# wav_file = '0000000.wav'
+
 for F in file_list:
     f = open(F)
     R = f.readlines()
@@ -14,4 +20,5 @@ for F in file_list:
     for i, r in enumerate(R):
         wav_file = r.split('|')[0]
         data, sampling_rate = librosa.core.load(wav_file, sr)
+        data = librosa.effects.trim(data, top_db= trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
         write(wav_file, sr, data*max_wav_value)

b007c5fbcb549e7fbc4ed7762f437ad4738b6c70
Yeongtae <hwak1234@hanmail.net>
2018-12-03 14:29:29 +0900

    Saving normalized wave Process to learn model without nomalizing wave

--- change_sr.py
+++ change_sr.py
@@ -1,3 +1,4 @@
+from scipy.io.wavfile import read
 from scipy.io.wavfile import write
 import librosa
 
@@ -11,14 +12,41 @@ trim_hop_size = 256
 trim_top_db = 23
 # wav_file = '0000000.wav'
 
-for F in file_list:
-    f = open(F)
-    R = f.readlines()
-    f.close()
-    print('='*5+F+'='*5)
+def run():
+    for F in file_list:
+        f = open(F)
+        R = f.readlines()
+        f.close()
+        print('='*5+F+'='*5)
 
-    for i, r in enumerate(R):
-        wav_file = r.split('|')[0]
-        data, sampling_rate = librosa.core.load(wav_file, sr)
-        data = librosa.effects.trim(data, top_db= trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
-        write(wav_file, sr, data*max_wav_value)
+        for i, r in enumerate(R):
+            wav_file = r.split('|')[0]
+            data, sampling_rate = librosa.core.load(wav_file, sr)
+            data = librosa.effects.trim(data, top_db= trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
+            write(wav_file, sr, data)
+
+if __name__ == "__main__":
+    # for test
+    # wav_file = '0000000.wav'
+    # data, sampling_rate = librosa.core.load(wav_file, sr)
+    # data_ = librosa.effects.trim(data, top_db=trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
+    # print(len(data), len(data_))
+    # write('test1.wav', sampling_rate, data)
+    # write('test2.wav', sampling_rate, data_)
+
+    # # for test 2
+    # wav_file = '0000000.wav'
+    # wav_file2 = 'test1.wav'
+    # wav_file3 = 'test2.wav'
+    #
+    # sr1, data1= read(wav_file)
+    # sr2, data2 = read(wav_file2)
+    # sr3, data3 = read(wav_file3)
+    #
+    # print('sr1:{}, sr2:{}, sr3:{}'.format(sr1, sr2, sr3))
+    # print('{} {} {}'.format(len(data1), len(data2), len(data3)))
+    # print('{} {} {}'.format(data1.min(), data2.min(), data3.min()))
+    # print('{} {} {}'.format(data1.max(), data2.max(), data3.max()))
+
+    # for run
+    run()

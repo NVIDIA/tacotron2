eff5cc1c90f29af663896976f0248637a1867c09
Yeongtae <hwak1234@hanmail.net>
2018-12-06 14:42:24 +0900

    add 0.5 silent.

--- change_sr.py
+++ change_sr.py
@@ -4,9 +4,10 @@ import librosa
 import numpy as np
 
 sr = 22050
+half_sr = int(sr/2)
 max_wav_value=32768.0
 file_list = ['filelists/nam-h_test_filelist.txt', 'filelists/nam-h_train_filelist.txt', 'filelists/nam-h_val_filelist.txt']
-
+#file_list = ['filelists/lj_audio_text_test_filelist.txt', 'filelists/lj_audio_text_train_filelist.txt', 'filelists/lj_audio_text_val_filelist.txt']
 # M-AILABS (and other datasets) trim params
 trim_fft_size = 1024
 trim_hop_size = 256
@@ -25,6 +26,7 @@ def run():
             data, sampling_rate = librosa.core.load(wav_file, sr)
             data = librosa.effects.trim(data, top_db= trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
             data = data*max_wav_value
+            data = np.append(data, [0.]*half_sr)
             data = data.astype(dtype=np.int16)
             write(wav_file, sr, data)
             if(i%100 == 0):
@@ -32,21 +34,21 @@ def run():
 
 if __name__ == "__main__":
     # for test
-    wav_file = '0000000.wav'
-    data, sampling_rate = librosa.core.load(wav_file, sr)
-    data_ = librosa.effects.trim(data, top_db=trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
-    print(len(data), len(data_))
-    write('test1.wav', sampling_rate, data)
-    write('test2.wav', sampling_rate, data_)
-
-    # for test 2
-    wav_file = '0000000.wav'
-    wav_file2 = 'test1.wav'
-    wav_file3 = 'test2.wav'
-
-    sr1, data1= read(wav_file)
-    sr2, data2 = read(wav_file2)
-    sr3, data3 = read(wav_file3)
+    # wav_file = '0000000.wav'
+    # data, sampling_rate = librosa.core.load(wav_file, sr)
+    # data_ = librosa.effects.trim(data, top_db=trim_top_db, frame_length=trim_fft_size, hop_length=trim_hop_size)[0]
+    # print(len(data), len(data_))
+    # write('test1.wav', sampling_rate, data)
+    # write('test2.wav', sampling_rate, data_)
+    #
+    # # for test 2
+    # wav_file = '0000000.wav'
+    # wav_file2 = 'test1.wav'
+    # wav_file3 = 'test2.wav'
+    #
+    # sr1, data1= read(wav_file)
+    # sr2, data2 = read(wav_file2)
+    # sr3, data3 = read(wav_file3)
     #
     # print('sr1:{}, sr2:{}, sr3:{}'.format(sr1, sr2, sr3))
     # print('{} {} {}'.format(len(data1), len(data2), len(data3)))

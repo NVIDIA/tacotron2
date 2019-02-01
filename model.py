fixing optimizer scaling going down to 1 in fp16 training

model.py
@@ -350,13 +350,17 @@ class Decoder(nn.Module):
         gate_output: gate output energies
         attention_weights:
         """
+        lstmcell_dtype = self.attention_hidden.dtype
+
         cell_input = torch.cat((decoder_input, self.attention_context), -1)
         self.attention_hidden, self.attention_cell = self.attention_rnn(
-            cell_input, (self.attention_hidden, self.attention_cell))
+            cell_input.float(), (self.attention_hidden.float(), self.attention_cell.float()))
         self.attention_hidden = F.dropout(
             self.attention_hidden, self.p_attention_dropout, self.training)
         self.attention_cell = F.dropout(
             self.attention_cell, self.p_attention_dropout, self.training)
+
+        self.attention_hidden = self.attention_hidden.to(lstmcell_dtype)
 
         attention_weights_cat = torch.cat(
             (self.attention_weights.unsqueeze(1),
@@ -369,11 +373,13 @@ class Decoder(nn.Module):
         decoder_input = torch.cat(
             (self.attention_hidden, self.attention_context), -1)
         self.decoder_hidden, self.decoder_cell = self.decoder_rnn(
-            decoder_input, (self.decoder_hidden, self.decoder_cell))
+            decoder_input.float(), (self.decoder_hidden.float(), self.decoder_cell.float()))
         self.decoder_hidden = F.dropout(
             self.decoder_hidden, self.p_decoder_dropout, self.training)
         self.decoder_cell = F.dropout(
             self.decoder_cell, self.p_decoder_dropout, self.training)
+
+        self.decoder_hidden = self.decoder_hidden.to(lstmcell_dtype)
 
         decoder_hidden_attention_context = torch.cat(
             (self.decoder_hidden, self.attention_context), dim=1)
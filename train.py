9ef9d7071ffbded7054e3972f76629e0b5b7a69a
jybaek <oops.jybaek@gmail.com>
2019-05-14 14:05:10 +0900

    Fixed cpu version training error

--- train.py
+++ train.py
@@ -71,7 +71,7 @@ def prepare_directories_and_logger(output_directory, log_directory, rank):
 
 
 def load_model(hparams):
-    model = Tacotron2(hparams).cuda()
+    model = Tacotron2(hparams).cuda() if torch.cuda.is_available() else Tacotron2(hparams)
     if hparams.fp16_run:
         model.decoder.attention_layer.score_mask_value = finfo('float16').min
 

e5c46f19583c7c8bc2c8a52b78349cf1e424ace4
Delgerdalai Sukhbaatar <delgerdalai@gmail.com>
2019-06-19 18:40:14 +0800

    sync upstream

--- hparams.py
+++ hparams.py
@@ -81,7 +81,7 @@ def create_hparams(hparams_string=None, verbose=False):
         learning_rate=1e-3,
         weight_decay=1e-6,
         grad_clip_thresh=1.0,
-        batch_size=64,
+        batch_size=36,
         mask_padding=True  # set model's padded outputs to padded values
     )
 

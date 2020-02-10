438b9399ffc903ff80ba9e3c70d09e2c269c157e
ntt123 <ntt123@earth>
2020-02-10 16:15:55 +0800

    remove tensorboardX; use torch.utils.tensorboard

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 import random
 import torch
-from tensorboardX import SummaryWriter
+from torch.utils.tensorboard import SummaryWriter
 from plotting_utils import plot_alignment_to_numpy, plot_spectrogram_to_numpy
 from plotting_utils import plot_gate_outputs_to_numpy
 
